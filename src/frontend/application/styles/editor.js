a389fdb2d64d39432e9234f403590f1815e95f9b
Jack <email@qiaohong.org>
2017-04-26 09:51:50 +0800

    separate editor style

---
+++ src/frontend/application/styles/editor.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Jack on 4/26/2017.
+ */
+
+module.exports = {
+    base: {
+        fontSize: '22px',
+
+        outlineColor: 'initial',
+        outlineStyle: 'none',
+        outlineWidth: 'initial',
+    },
+    title: {
+        width: '100%',
+
+        textAlign: 'center',
+        fontSize: '1.7em',
+        fontWeight: 'bold',
+
+        border: 'none',
+        borderBottom: '1px solid #dbdbdb'
+
+    },
+    content: {
+        margin: 'auto',
+        width: '61.8%',
+
+        lineHeight: '30px',
+    }
+};
--- src/frontend/application/styles/index.js
+++ src/frontend/application/styles/index.js
@@ -3,34 +3,7 @@
  */
 
 let styles = {
-    editor: {
-        base: {
-            fontSize: '22px',
-
-            outlineColor: 'initial',
-            outlineStyle: 'none',
-            outlineWidth: 'initial',
-        },
-        title: {
-            width: '100%',
-
-            textAlign: 'center',
-            fontSize: '1.7em',
-            fontWeight: 'bold',
-
-            border: 'none',
-            borderBottom: '1px solid #dbdbdb'
-
-        },
-        content: {
-            margin: 'auto',
-            width: '61.8%',
-
-            lineHeight: '30px',
-        }
-    },
-
-
+    editor: require('./editor')
 };
 
 export default styles;
