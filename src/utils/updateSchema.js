6863f96f55588709e44eec201c97c56d11e39a53
Jack <email@qiaohong.org>
2017-05-20 22:09:07 +0800

    generate schema

--- src/utils/updateSchema.js
+++ src/utils/updateSchema.js
@@ -1,10 +1,10 @@
 import fs from 'fs'
 import path from 'path'
-import graphql from 'graphql'
-import introspectionQuery from 'graphql/utilities'
+import {graphql} from 'graphql';
+import {introspectionQuery} from 'graphql/utilities'
 import schema from '../backend/api/graphql/schema'
 
 graphql(schema, introspectionQuery).then(function (result) {
     if (result.errors) return console.error(result.errors);
-    fs.writeFileSync(path.join(__dirname, '../schema/schema.json'), JSON.stringify(result, null, 2))
+    fs.writeFileSync(path.join(__dirname, '../../graphql.schema.json'), JSON.stringify(result, null, 2))
 });
