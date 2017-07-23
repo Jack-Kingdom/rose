872db9dda402afceb6ae85d418cd5d2cfb464475
Jack King <email@qiaohong.org>
2017-07-23 07:41:51 -0700

    resolve some merge bug

--- src/backend/logic/router/spider.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Jack on 4/12/2017.
- */
-
-// TODO staticRouter render html to spider, such as: Baidu Spider, Google Spider
--- src/frontend/application/index.js
+++ src/frontend/application/index.js
@@ -2,10 +2,10 @@
  * Created by Jack on 4/1/2017.
  */
 
-// import React from 'react';
-// import {BrowserRouter, Route, Link} from 'react-router-dom'
-// import Home from './home'
-// import Admin from './admin'
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+import Home from './home'
+import Admin from './admin'
 
 class App extends React.Component {
 
@@ -14,6 +14,7 @@ class App extends React.Component {
         this.status = {
         }
     }
+
     render() {
         return (
             <BrowserRouter>
@@ -71,58 +72,58 @@ export default App;
 //     }
 // }
 
-import Relay, {QueryRenderer, graphql,} from 'react-relay';
-import {Environment, Network}  from 'relay-runtime';
-
-// Define a function that fetches the results of an operation (query/mutation/etc)
-// and returns its results as a Promise:
-function fetchQuery(operation,
-                    variables,
-                    cacheConfig,
-                    uploadables,) {
-    return fetch('/graphql', {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        }, // Add authentication and other headers here
-        body: JSON.stringify({
-            query: operation.text, // GraphQL text from input
-            variables,
-        }),
-    }).then(response => {
-        return response.json();
-    });
-}
-
-// Create a network layer from the fetch function
-const network = Network.create(fetchQuery);
-
-// Create an environment using this network:
-const environment = new Environment({
-    // other options
-    network,
-});
-
-
-
-export default <QueryRenderer
-    environment={environment}
-    query={graphql`
-    query {
-        articles{
-        title
-        }
-    }
-  `}
-    variables={{
-        ID: '592067cb58af1e1008200166',
-    }}
-    render={(error, props) => {
-        if (error) {
-            return <div>{error.message}</div>;
-        } else if (props) {
-            return <div>{props.article.title} is great!</div>;
-        }
-        return <div>Loading</div>;
-    }}
-/>;
+// import Relay, {QueryRenderer, graphql,} from 'react-relay';
+// import {Environment, Network}  from 'relay-runtime';
+//
+// // Define a function that fetches the results of an operation (query/mutation/etc)
+// // and returns its results as a Promise:
+// function fetchQuery(operation,
+//                     variables,
+//                     cacheConfig,
+//                     uploadables,) {
+//     return fetch('/graphql', {
+//         method: 'POST',
+//         headers: {
+//             'content-type': 'application/json'
+//         }, // Add authentication and other headers here
+//         body: JSON.stringify({
+//             query: operation.text, // GraphQL text from input
+//             variables,
+//         }),
+//     }).then(response => {
+//         return response.json();
+//     });
+// }
+//
+// // Create a network layer from the fetch function
+// const network = Network.create(fetchQuery);
+//
+// // Create an environment using this network:
+// const environment = new Environment({
+//     // other options
+//     network,
+// });
+//
+//
+//
+// export default <QueryRenderer
+//     environment={environment}
+//     query={graphql`
+//     query {
+//         articles{
+//         title
+//         }
+//     }
+//   `}
+//     variables={{
+//         ID: '592067cb58af1e1008200166',
+//     }}
+//     render={(error, props) => {
+//         if (error) {
+//             return <div>{error.message}</div>;
+//         } else if (props) {
+//             return <div>{props.article.title} is great!</div>;
+//         }
+//         return <div>Loading</div>;
+//     }}
+// />;
