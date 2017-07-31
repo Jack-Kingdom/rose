2c57b05c77fae23c20b8a1302b7b1c5cf1e88d0b
jack <email@qiaohong.org>
2017-07-31 16:42:47 +0800

    add media to router

---
+++ src/api/media/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+import express from 'express';
+
+const mediaRouter = express.Router();
+
+mediaRouter.post('/upload', (req, res) => {
+
+});
+
+//todo
+mediaRouter.get('slug',(req,res)=>{
+
+});
+
+export default mediaRouter;
--- src/logic/router/index.js
+++ src/logic/router/index.js
@@ -4,11 +4,13 @@ import express from 'express';
 import authRouter from '../../api/auth';
 import restfulRouter from '../../api/restful';
 import graphqlRouter from '../../api/graphql';
+import mediaRouter from '../../api/media';
 
 const router = express.Router();
 
-router.use('/api/auth',authRouter);
-router.use('/api/v1',restfulRouter);
-router.use('/api/graphql',graphqlRouter);
+router.use('/api/auth', authRouter);
+router.use('/api/v1', restfulRouter);
+router.use('/api/graphql', graphqlRouter);
+router.use('/api/media', mediaRouter);
 
 export default router;
