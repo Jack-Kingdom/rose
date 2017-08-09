8eb666bd5574d6d1b97183c0b8add8f83db3f2fe
jack <email@qiaohong.org>
2017-08-09 10:14:24 +0800

    add meta article template

---
+++ src/meta/article.js
@@ -0,0 +1,28 @@
+"use strict";
+
+import models from '../persistence/models';
+
+class Article {
+
+    static async createArticle() {
+
+    }
+
+    static async deleteArticle() {
+
+    }
+
+    static async updateArticle() {
+
+    }
+
+    static async queryArticle() {
+
+    }
+
+    static async queryMultiArticle() {
+
+    }
+}
+
+export default Article;
