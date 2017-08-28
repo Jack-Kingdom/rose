1b03510ca088fd7a98bbde5a75812240d9d65375
jack <email@qiaohong.org>
2017-08-28 18:30:31 +0800

    try to rewrite query as class

--- src/api/graphql/v1/root/query/article.js
+++ src/api/graphql/v1/root/query/article.js
@@ -1,12 +1,32 @@
 import Ops from '../../../../../ops'
+import CategoryQuery from './category'
+import TagQuery from './tag'
+
+class Article {
+  constructor (article) {
+    // article.forEach((attr) => this[attr] = article[attr])
+    // article.forEach((attr) => console.log(attr))
+    this.slug = article.slug
+    this.title = article.title
+    // this.tags = [{slug:"a",title:"hello"}]
+
+  }
+
+  tags (args, req, node) {
+    console.log(node)
+  }
+}
 
 export default {
   articles: async (args, req, node) => {
     // todo tag and category's articles
+    // articles.forEach((article) => article.tags.map((slug) => Ops.Tag.retrieve(req, slug)))
     return Ops.Article.multipleRetrieve(req, args.order, args.offset, args.limit, args.status)
   },
 
-  article: (args, req, node) => {
-    return Ops.Article.retrieve(req, args.slug)
+  article: async (args, req, node) => {
+    // let article = await Ops.Article.retrieve(req, args.slug)
+    // return article
+    return new Article(await Ops.Article.retrieve(req, args.slug))
   }
 }
--- src/api/graphql/v1/root/query/tag.js
+++ src/api/graphql/v1/root/query/tag.js
@@ -3,12 +3,12 @@ import Ops from '../../../../../ops'
 export default {
   tags: (args, req, node) => {
     // todo tag and category's articles
-    console.log(node)
+    console.log('tags queried')
     return Ops.Tag.multipleRetrieve(req, args.order, args.offset, args.limit)
   },
 
   tag: (args, req, node) => {
-    console.log(node)
+    console.log('tags queried')
     return Ops.Tag.retrieve(req, args.slug)
   }
 }
