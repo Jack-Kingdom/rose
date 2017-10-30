df415bcf63f6f68f81290c5a5c524a738a139016
Jack King <email@qiaohong.org>
2017-10-30 14:15:59 +0800

    add session plugin

--- config/development.js
+++ config/development.js
@@ -1,10 +1,11 @@
 module.exports = {
     port: 3000,
     session_secret: 'secret string',
-    mongodbUrl: 'mongodb://192.168.1.107:27017/DevRose',
+    // mongodbUrl: 'mongodb://192.168.1.107:27017/DevRose',
+    mongodbUrl: 'mongodb://192.168.4.60:27017/DevRose',
     openRegister: true,
     graphqlMaxDepth: 1,
 
-    // unit: second, value: 1 week
-    sessionTimeout: 7 * 24 * 60 * 60
+    // value: 1 week
+    sessionTimeout: 7 * 24 * 60 * 60 * 1000
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const Koa = require("koa");
+const session = require("koa-session");
 const config = require("../config");
 const apiRouter = require("./api/index");
 
 const app = new Koa();
+app.keys = [config.session_secret];
+app.use(session({maxAge: config.sessionTimeout}, app));
 
 app.use(apiRouter.routes());
 
