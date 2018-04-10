d7e8a2c3ffe96e7f28951ef21f12bc7992e19147
Jack King <email@qiaohong.org>
2018-04-10 15:28:34 +0800

    save

--- test/test.js
+++ test/test.js
@@ -1,8 +1,19 @@
-let developmentConfig = {
-  debug: true,
-  mongodbUrl: 'mongodb://localhost:27017/RoseTest'
-}
+const mysql = require('mysql');
+let pool = mysql.createPool({
+    connectionLimit: 25,
+    host: 'localhost',
+    user: 'root',
+    password: 'qiaohong',
+    database: 'rose'
+});
 
-for (let ele in developmentConfig) {
-  console.log(ele)
-}
+pool.query('INSERT account VALUE (NULL, \'sdfjlsjfklsdjfl\', \'email@qiaohong.org\', \'qiaohong\', \'Jack\',\'master\',FALSE, NULL, NULL, NULL);', function (error, results, fields) {
+    if (error) throw error;
+
+    // console.log('----------');
+    // console.log(results);
+    // console.log('----------');
+    // console.log(results[0].username);
+    // console.log('-------');
+    console.log(fields);
+});
